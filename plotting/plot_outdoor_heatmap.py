913599d4b19d4dd19b5d6f9cd153f11f2873c0c4
Gilles Callebaut <callebaut.gilles@gmail.com>
2020-08-21 16:20:42 +0200

    update plt

--- plotting/plot_outdoor_heatmap.py
+++ plotting/plot_outdoor_heatmap.py
@@ -76,22 +76,6 @@ if __name__ == '__main__':
         grid_z1 = griddata(points, power_values, (grid_x, grid_y), method='linear')
         grid_z2 = griddata(points, power_values, (grid_x, grid_y), method='cubic')
 
-        plt.subplot(221)
-        img = plt.imread(pjoin(current_path, "..", "img", "map-heatmap", "map.png"))
-        fig, ax = plt.subplots()
-        ax.imshow(img, extent=[0, 1, 0, 1])
-        plt.subplot(222)
-        plt.imshow(grid_z0.T, extent=(0, 1, 0, 1), origin='lower')
-        plt.title('Nearest')
-        plt.subplot(223)
-        plt.imshow(grid_z1.T, extent=(0, 1, 0, 1), origin='lower')
-        plt.title('Linear')
-        plt.subplot(224)
-        plt.imshow(grid_z2.T, extent=(0, 1, 0, 1), origin='lower')
-        plt.title('Cubic')
-        plt.gcf().set_size_inches(6, 6)
-        plt.show()
-
         plt.cla()
         fig, ax = plt.subplots()
         #plt.imshow(img, extent=[0, 1, 0, 1])
@@ -110,7 +94,7 @@ if __name__ == '__main__':
         #plt.imshow(img, origin = 'lower',  extent = [0, img.shape[0], 0, img.shape[1]], aspect = 1000)
         a = grid_z1.T
         a = np.ma.array(a, mask=np.isnan(a))
-        plt.imshow(a, extent=[0, 1, 0, 1], origin='lower')
+        plt.imshow(a, extent=[0, 1, 0, 1], origin='lower', cmap='viridis_r')
         plt.colorbar()
         plt.savefig(pjoin(current_path,'heatmap_median_evm.pdf'))
 
