f1c135d1ee038e5dda728827f435f3c32d3dee99
Gilles Callebaut <callebaut.gilles@gmail.com>
2020-08-22 22:51:59 +0200

    Update plot_outdoor_heatmap.py

--- plotting/plot_outdoor_heatmap.py
+++ plotting/plot_outdoor_heatmap.py
@@ -8,6 +8,16 @@ import numpy as np
 import pandas as pd
 from scipy.interpolate import griddata
 from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import folium
+import branca
+from folium import plugins
+import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
+import geojsoncontour
+import scipy as sp
+import scipy.ndimage
 
 from utils import util_loc
 
@@ -101,32 +111,90 @@ if __name__ == '__main__':
             grid_h = griddata((points_x[conf], points_y[conf]), power_values[conf], (grid_x, grid_y), method='linear')
             grid_evm = griddata((points_x[conf], points_y[conf]), evm_values[conf], (grid_x, grid_y), method='linear')
 
-            circle_radius = (np.max(x_arr) - np.min(x_arr)) / 100
+            contourf = plt.contourf(grid_x, grid_x, grid_h, alpha=0.5, linestyles='None',
+                                    vmin=power_min, vmax=power_max)
 
-            circles = [plt.Circle((p_x, p_y), circle_radius, fill=False) for p_x, p_y in
-                       zip(points_x[conf], points_y[conf])]
+            # Convert matplotlib contourf to geojson
+            geojson = geojsoncontour.contourf_to_geojson(
+                contourf=contourf,
+                min_angle_deg=3.0,
+                ndigits=5,
+                stroke_width=1,
+                fill_opacity=0.5)
 
-            plt.cla()
-            fig, ax = plt.subplots()
-            # plt.imshow(img, extent=[0, 1, 0, 1])
+
+            center = df.iloc[0]
+
+            m = folium.Map(
+                location=[center["latitude"], center["longitude"]],
+                zoom_start=18,
+            )
+
+            colors = {
+                "BS": "black",
+                "A": "red",
+                "B": "blue",
+                "C": "green",
+                "D": "purple"
+            }
+
+            for index, row in df.iterrows():
+                popup = folium.Popup(
+                    f'<a href="https://dramco.be/projects/marrmot/balcony/measurements/{row["path"]}-{int(row["point"])}-a-ULA-868/snapshots.html" target="_blank">{row["location"]} ULA</a><br>'
+                    f'<a href="https://dramco.be/projects/marrmot/balcony/measurements/{row["path"]}-{int(row["point"])}-a-URA-868/snapshots.html" target="_blank">{row["location"]} URA</a>')
+                # do not show popup with BS
+                if index == 0:
+                    popup = None
+
+                folium.Marker(
+                    icon=folium.Icon(color=colors[row["path"]]),
+                    location=[row["latitude"], row["longitude"]],
+                    popup=popup
+                ).add_to(m)
+
+            # Plot the contour plot on folium
+            folium.GeoJson(
+                geojson,
+                style_function=lambda x: {
+                    'color': x['properties']['stroke'],
+                    'weight': x['properties']['stroke-width'],
+                    'fillColor': x['properties']['fill'],
+                    'opacity': 0.6,
+                }).add_to(m)
+
+            # Fullscreen mode
+            plugins.Fullscreen(position='topright', force_separate_button=True).add_to(geomap)
+
+            # Plot the data
+            m.save(pjoin(current_path, f'heatmap_median_evm_{conf}.html'))
+
+
+            # circle_radius = (np.max(x_arr) - np.min(x_arr)) / 100
+            #
+            # circles = [plt.Circle((p_x, p_y), circle_radius, fill=False) for p_x, p_y in
+            #            zip(points_x[conf], points_y[conf])]
+            #
+            # plt.cla()
+            # fig, ax = plt.subplots()
+            # # plt.imshow(img, extent=[0, 1, 0, 1])
+            # # [ax.add_artist(c) for c in circles]
+            #
+            # a = grid_h
+            # a = np.ma.array(a, mask=np.isnan(a))
+            # plt.imshow(a, extent=[np.min(x_arr), np.max(x_arr), np.min(y_arr), np.max(y_arr)], origin='lower',
+            #            vmin=power_min, vmax=power_max)
+            # # [plt.scatter(0, 0, s=4000) for p_x, p_y in zip(points_x[conf], points_y[conf])]
             # [ax.add_artist(c) for c in circles]
-
-            a = grid_h
-            a = np.ma.array(a, mask=np.isnan(a))
-            plt.imshow(a, extent=[np.min(x_arr), np.max(x_arr), np.min(y_arr), np.max(y_arr)], origin='lower',
-                       vmin=power_min, vmax=power_max)
-            # [plt.scatter(0, 0, s=4000) for p_x, p_y in zip(points_x[conf], points_y[conf])]
-            [ax.add_artist(c) for c in circles]
-            plt.colorbar()
-            plt.savefig(pjoin(current_path, f'heatmap_median_h_{conf}.pdf'))
-
-            plt.cla()
-            plt.subplots()
-            # plt.imshow(img, extent=[0, 1, 0, 1])
-            a = grid_evm
-            a = np.ma.array(a, mask=np.isnan(a))
-            plt.imshow(a, extent=[np.min(x_arr), np.max(x_arr), np.min(y_arr), np.max(y_arr)], origin='lower',
-                       cmap='viridis_r', vmin=evm_min, vmax=evm_max)
-            [ax.add_artist(c) for c in circles]
-            plt.colorbar()
-            plt.savefig(pjoin(current_path, f'heatmap_median_evm_{conf}.pdf'))
+            # plt.colorbar()
+            # plt.savefig(pjoin(current_path, f'heatmap_median_h_{conf}.pdf'))
+            #
+            # plt.cla()
+            # plt.subplots()
+            # # plt.imshow(img, extent=[0, 1, 0, 1])
+            # a = grid_evm
+            # a = np.ma.array(a, mask=np.isnan(a))
+            # plt.imshow(a, extent=[np.min(x_arr), np.max(x_arr), np.min(y_arr), np.max(y_arr)], origin='lower',
+            #            cmap='viridis_r', vmin=evm_min, vmax=evm_max)
+            # [ax.add_artist(c) for c in circles]
+            # plt.colorbar()
+            # plt.savefig(pjoin(current_path, f'heatmap_median_evm_{conf}.pdf'))
