1c373a4e9606ef81fd64dc81fe4efc756681dd87
Gilles Callebaut <callebaut.gilles@gmail.com>
2020-09-30 08:31:44 +0200

    updated aoa

--- processing/aoa_algorithms.py
+++ processing/aoa_algorithms.py
@@ -23,15 +23,14 @@ def music(cov_mat, K, M, angles):
         av = array_response_vector(array, angles[i])
         pspectrum[i] = 1 / LA.norm((Qn.conj().transpose() @ av))
     psindB = np.log10(10 * pspectrum / pspectrum.min())
-    # DoAsMUSIC, _ = ss.find_peaks(psindB, height=1.35, distance=1.5)
-    return pspectrum, psindB
+    peaks, _ = ss.find_peaks(psindB, height=1.35, distance=1.5)
+    return pspectrum, psindB, peaks
 
 
 def esprit(cov_mat, K, M):
     # cov_mat is the signal covariance matrix, K is the number of sources, M is the number of antennas
     _, U = LA.eig(cov_mat)
     S = U[:, 0:K]
-    phi = LA.pinv(S[0:M - 1]) @ S[
-                                1:M]  # the original array is divided into two subarrays [0,1,...,N-2] and [1,2,...,N-1]
+    phi = LA.pinv(S[0:M - 1]) @ S[1:M]  # the original array is divided into two subarrays [0,1,...,N-2] and [1,2,...,N-1]
     eigs, _ = LA.eig(phi)
     return np.arcsin(np.angle(eigs) / np.pi)
--- processing/compute_aoa.py
+++ processing/compute_aoa.py
@@ -17,13 +17,22 @@ if __name__ == '__main__':
     for d in dirs:
         if is_868(d) and not is_cont_meas(d) and is_ula(d):
             H = np.load(os.path.join(root_dir, d, "small-channel.npy"))
-            # average over all snapshots
-            H_avg = np.mean(H, axis=0)
+            H =H[:, :, 0:31]
+            # average over freq points
+            H_avg = np.mean(H, axis=1)
+            H_avg = H_avg.transpose() # so it is a MxN matrix
+            cov_mat = H_avg@H_avg.conj().transpose()
             M = H.shape[2]
             angles = np.linspace(-np.pi / 2, np.pi / 2, 360)
-            pspectrum, psindB = alg.music(np.cov(H_avg), 1, M, angles)
+            pspectrum, psindB, peaks = alg.music(cov_mat, 1, M, angles)
+            plt.cla()
             plt.plot(angles, psindB)
-            plt.save(os.path.join(root_dir, d, "power_spectrum.png"))
+            plt.plot(angles[peaks], psindB[peaks], 'x')
+            plt.legend(['pseudo spectrum', 'Estimated DoAs'])
+            plt.savefig(os.path.join(root_dir, d, "power_spectrum.png"))
+
+            if len(peaks) > 0:
+                print(d)
 
 
 
